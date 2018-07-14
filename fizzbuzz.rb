b79e73a1f50db281fb305bb874c1eca73ae94a27
Benjamin Jones <bljones36@gmail.com>
2018-07-14 13:19:59 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 require 'pry'
 def fizzbuzz(x)
-  if x % 3 == 0
+  if (x % 5 == 0 && x % 3 == 0)
+    "FizzBuzz"
+  elsif x % 3 == 0
     "Fizz"
   elsif x % 5 == 0
     "Buzz"
-  elsif (x % 5 == 0 && x % 3 == 0)
-    "FizzBuzz"
   else 
     nil
   end
